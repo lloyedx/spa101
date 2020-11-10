d94688a042e75e14d72acd9df5127dd017612ccf
lloyd <lloydxuyan@gmail.com>
2020-11-10 21:31:01 +0800

    4.5.1    the removeSlider method

--- lab/ch04/spa/js/spa.chat.js
+++ lab/ch04/spa/js/spa.chat.js
@@ -289,6 +289,35 @@ spa.chat = (function() {
   };
   // End public method /setSliderPosition/
 
+  // Begin public method /removeSlider/
+  // Purpose:
+  //   * Removes chatSlider DOM element
+  //   * Reverts to initial state
+  //   * Removes pointers to callbacks and other data
+  // Arguments: none
+  // Returns: true
+  // Throws: none
+  //
+  removeSlider = function() {
+    // unwind initialization and state
+    // remove DOM container; this remove event bindings too
+    if (jqueryMap.$slider)
+    {
+      jqueryMap.$slider.remove();
+      jqueryMap = {};
+    }
+    stateMap.$append_target = null;
+    stateMap.position_type = 'closed';
+
+    // unwind key configurations
+    configMap.chat_model = null;
+    configMap.people_model = null;
+    configMap.set_chat_anchor = null;
+
+    return true;
+  };
+  // End public method /removeSlider/
+
   // return public methods
   return {
     configModule: configModule,
